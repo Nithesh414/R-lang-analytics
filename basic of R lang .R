f73488417067053cd8e061414aec44f9245dc096
Nithesh <selvarajselvaraj9995@gmail.com>
2026-02-04 20:40:37 +0530

    Create basic of R lang .R
    this is code for basic funtion declaration and calling of function in r language

---
+++ basic of R lang .R
@@ -0,0 +1,15 @@
+print("hello world")
+x<-9
+z<-10
+add <- function(){
+  result <- a+b
+  return(result)
+}
+print(add())
+
+nameadd <-function(fname,lname){
+  a <<- 10
+  paste(fname,lname)
+}
+nameadd("ranbir","kapoor")
+print(a)
