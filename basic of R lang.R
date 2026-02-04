af21c70997140c4c32d7197c78722c841476a271
Nithesh <selvarajselvaraj9995@gmail.com>
2026-02-04 20:39:16 +0530

    Create basic of R lang.R
    basic variable declaration and data type conversion in r language

---
+++ basic of R lang.R
@@ -0,0 +1,17 @@
+lengt<- 10
+breadth <- 20
+c<- lengt * breadth
+paste("area of rectangle is ",c)
+x<-10
+y<-20
+c<-x+y
+c
+d<-c-x
+d
+e<-as.character(d)
+class(e)
+c<-"hii
+this 
+"
+cat(c)
+class(c)
